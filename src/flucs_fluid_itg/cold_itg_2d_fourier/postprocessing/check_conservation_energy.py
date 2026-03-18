9b9c948e42f44bcaf2d18d388621be62847d9b2b
physycola <toby.adkins1@gmail.com>
2026-03-17 18:28:29 -0700

    Reflecting postprocessing changes to support multiple output files

--- src/flucs_fluid_itg/cold_itg_2d_fourier/postprocessing/check_conservation_energy.py
+++ src/flucs_fluid_itg/cold_itg_2d_fourier/postprocessing/check_conservation_energy.py
@@ -7,7 +7,7 @@ from flucs.postprocessing import FlucsPostProcessing
 def free_energy_check(post):
 
     # Get valid files for the specified variable
-    nc_paths = post.get_valid_files("free_energy/dWdt")
+    nc_paths = post.get_valid_netcdf_paths("free_energy/dWdt")
 
     # Iterate over output files
     for index, nc_path in enumerate(nc_paths):
@@ -24,11 +24,11 @@ def free_energy_check(post):
         variables = post.get_netcdf_variables(nc_path)
 
         time, boundaries, _ = post.load_netcdf_variable(nc_path, "time")
-        dt, _, _ = post.load_netcdf_variable(nc_path, "dt")
-        free_energy, _, _ = post.load_netcdf_variable(nc_path, "free_energy/W")
-        dWdt, _, _ = post.load_netcdf_variable(nc_path, "free_energy/dWdt")
-        injection, _, _ = post.load_netcdf_variable(nc_path, "free_energy/dWdt_inj")
-        dissipation, _, _ = post.load_netcdf_variable(nc_path, "free_energy/dWdt_coll")
+        dt = post.load_netcdf_variable(nc_path, "dt")[0]
+        free_energy = post.load_netcdf_variable(nc_path, "free_energy/W")[0]
+        dWdt= post.load_netcdf_variable(nc_path, "free_energy/dWdt")[0]
+        injection = post.load_netcdf_variable(nc_path, "free_energy/dWdt_inj")[0]
+        dissipation = post.load_netcdf_variable(nc_path, "free_energy/dWdt_coll")[0]
 
         # Add hyperdissipation
         for variable in variables:
@@ -84,7 +84,7 @@ if __name__ == "__main__":
     post = FlucsPostProcessing(
         io_paths=args.io_path,
         save_directory=args.save_directory,
-        output_file="output.0d.nc",
+        output_files=["output.0d.nc"],
         constraint="both"
     )
 
